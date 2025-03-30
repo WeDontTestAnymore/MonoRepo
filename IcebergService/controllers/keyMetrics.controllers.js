d4babfd2bc6542ba708e7f9ac8d9103129f78fa4
Om Kulkarni <127368012+om7057@users.noreply.github.com>
2025-03-30 16:05:15 +0530

    keyMetrics_changes

--- IcebergService/controllers/keyMetrics.controllers.js
+++ IcebergService/controllers/keyMetrics.controllers.js
@@ -1,10 +1,10 @@
 import { DuckDBInstance } from "@duckdb/node-api";
-
+import { initializeDuckDBWithS3 } from "../utils/duck-db.js";
 import { filesize } from "filesize";
 
-//config contains minio access details and tables3uri contains location for table to fetch metadta
+//config contains minio access details and icebergPath contains location for table to fetch metadta
 
-const findTableDetails = async (config, tables3uri) => {
+const findTableDetails = async (config, icebergPath) => {
     const instance = await DuckDBInstance.create(':memory:');
     const connection = await instance.connect();
 
@@ -43,14 +43,14 @@ const findTableDetails = async (config, tables3uri) => {
         SELECT file_name, 
                SUM(row_group_num_rows) AS total_rows,
                SUM(total_compressed_size) as size_bytes
-        FROM parquet_metadata('${tables3uri}/data/*.parquet')
+        FROM parquet_metadata('${icebergPath}/data/*.parquet')
         GROUP BY file_name;
     `;
 
 
         const result = await connection.runAndReadAll(query);
         const rows = result.getRowObjectsJson();
-        console.log(rows);
+        
 
         return rows;
 
@@ -61,38 +61,17 @@ const findTableDetails = async (config, tables3uri) => {
 
 
 
-export const getTotalRows = async (req, res) => {
-    try {
-        const { config, tables3uri } = req.body;
 
-        if (!config || !tables3uri) {
-            return res.status(400).json({ error: "Missing config or tables3uri in request body" });
-        }
-
-        const result = await findTableDetails(config, tables3uri);
-        const totalRows = result.reduce((sum, result) => sum + Number(result.total_rows), 0);
-
-
-        if (!result) {
-            return res.status(500).json({ error: "Failed to fetch table details" });
-        }
-
-        res.status(200).json({ totalRows: totalRows });
-    } catch (error) {
-        console.error("Error in getTotalRows:", error.message);
-        res.status(500).json({ error: "Internal server error" });
-    }
-}
 
 export const getTotalFileSize = async (req, res) => {
     try {
-        const { config, tables3uri } = req.body;
+        const { config, icebergPath } = req.body;
 
-        if (!config || !tables3uri) {
-            return res.status(400).json({ error: "Missing config or tables3uri in request body" });
+        if (!config || !icebergPath) {
+            return res.status(400).json({ error: "Missing config or icebergPath in request body" });
         }
 
-        const result = await findTableDetails(config, tables3uri);
+        const result = await findTableDetails(config, icebergPath);
         const totalFileSize = result.reduce((sum, result) => sum + result.size_bytes, 0);
 
         if (!result) {
@@ -107,13 +86,13 @@ export const getTotalFileSize = async (req, res) => {
 
 export const getFileData = async (req, res) => {
     try {
-        const { config, tables3uri } = req.body;
+        const { config, icebergPath } = req.body;
 
-        if (!config || !tables3uri) {
-            return res.status(400).json({ error: "Missing config or tables3uri in request body" });
+        if (!config || !icebergPath) {
+            return res.status(400).json({ error: "Missing config or icebergPath in request body" });
         }
 
-        const result = await findTableDetails(config, tables3uri);
+        const result = await findTableDetails(config, icebergPath);
 
         if (!result || !Array.isArray(result)) {
             return res.status(500).json({ error: "Failed to fetch table details" });
@@ -134,13 +113,13 @@ export const getFileData = async (req, res) => {
 
 export const getOverhead = async (req, res) => {
     try {
-        const { config, tables3uri } = req.body;
+        const { config, icebergPath } = req.body;
 
-        if (!config || !tables3uri) {
-            return res.status(400).json({ error: "Missing config or tables3uri in request body" });
+        if (!config || !icebergPath) {
+            return res.status(400).json({ error: "Missing config or icebergPath in request body" });
         }
 
-        const result = await findTableDetails(config, tables3uri);
+        const result = await findTableDetails(config, icebergPath);
 
         if (!result) {
             return res.status(500).json({ error: "Failed to fetch table details" });
@@ -166,5 +145,5 @@ const config = {
     endpoint: "127.0.0.1:9000"
 };
 
-const tables3uri = "s3://iceberg-test/employment_data";
-// findTableDetails(config, tables3uri)
+const icebergPath = "s3://iceberg-test/employment_data";
+// findTableDetails(config, icebergPath)
