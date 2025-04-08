b88d383a0e577ff210f6b1ef01f2bc4e84945cf4
meethereum <meethbackup@gmail.com>
2025-04-09 00:53:25 +0530

    implemented total rows and total file size functionality

--- IcebergService/controllers/keyMetrics.controllers.js
+++ IcebergService/controllers/keyMetrics.controllers.js
@@ -4,9 +4,14 @@ import { filesize } from "filesize";
 
 //config contains minio access details and icebergPath contains location for table to fetch metadta
 
-const findTableDetails = async (config, icebergPath) => {
+
+
+const findAllTableDetails = async (config, icebergPath) => {
     const instance = await DuckDBInstance.create(':memory:');
     const connection = await instance.connect();
+
+    var totalSizeInBytes = 0;
+    var totalRows = 0;
 
     if (!config.key) {
         return console.error("s3 key not provided");
@@ -54,12 +59,15 @@ const findTableDetails = async (config, icebergPath) => {
         const resultArray = [];
 
         for (const row of rows) {
+          
             const filePath = row.file_path;
 
             const innerquery = `
            SELECT 
   '${filePath}' AS file_path,
-  SUM(row_group_num_rows) AS total_rows,
+  SUM(row_group_num_rows) AS row_count,
+  SUM(total_compressed_size) AS size_bytes,
+
   CASE
     WHEN SUM(total_compressed_size) >= 1073741824 THEN ROUND(SUM(total_compressed_size) / 1073741824.0, 2) || ' GB'
     WHEN SUM(total_compressed_size) >= 1048576 THEN ROUND(SUM(total_compressed_size) / 1048576.0, 2) || ' MB'
@@ -67,46 +75,25 @@ const findTableDetails = async (config, icebergPath) => {
     ELSE SUM(total_compressed_size) || ' B'
   END AS file_size
 FROM parquet_metadata('${filePath}')
-
           `;
 
             const res = await connection.runAndReadAll(innerquery);
             const parquetRows = res.getRowObjectsJson();
-            //console.log(parquetRows);  
-            resultArray.push(parquetRows);
-        }
+            //console.log(parquetRows);
 
-        console.log("resultant array is : ", resultArray);
-        return resultArray
+            resultArray.push(parquetRows[0]);
+            totalRows += Number(parquetRows[0].row_count);
+            totalSizeInBytes += Number(parquetRows[0].size_bytes);
+        }
+        const totalFileSize = filesize(totalSizeInBytes, { standard: "jedec" });
+        //console.log(resultArray,totalRows,totalFileSize);
+        return { resultArray, totalRows, totalFileSize };
     } catch (error) {
         console.log(error.message);
     }
 };
 
 
-
-
-
-export const getTotalFileSize = async (req, res) => {
-    try {
-        const { config, icebergPath } = req.body;
-
-        if (!config || !icebergPath) {
-            return res.status(400).json({ error: "Missing config or icebergPath in request body" });
-        }
-
-        const result = await findTableDetails(config, icebergPath);
-        const totalFileSize = result.reduce((sum, result) => sum + result.size_bytes, 0);
-
-        if (!result) {
-            return res.status(500).json({ error: "Failed to fetch table details" });
-        }
-        res.status(200).json({ totalFileSize: filesize(totalFileSize, { standard: "jedec" }) });
-    } catch (error) {
-        console.error("Error in getTotalFileSize:", error.message);
-        res.status(500).json({ error: "Internal server error" });
-    }
-}
 
 export const getFileData = async (req, res) => {
     try {
@@ -116,13 +103,19 @@ export const getFileData = async (req, res) => {
             return res.status(400).json({ error: "Missing config or icebergPath in request body" });
         }
 
-        const fileData = await findTableDetails(config, icebergPath);
+        const fileData = await findAllTableDetails(config, icebergPath);
 
-        if (!fileData || !Array.isArray(fileData)) {
+        console.log(fileData);
+
+        if (!fileData) {
             return res.status(500).json({ error: "Failed to fetch table details" });
         }
 
-        res.status(200).json({ fileData });
+        
+
+        const { resultArray, totalRows, totalFileSize } = fileData;
+
+        res.status(200).json({ fileData: resultArray, totalRows, totalFileSize });
     } catch (error) {
         console.error("Error in getFileData:", error.message);
         res.status(500).json({ error: "Internal server error" });
@@ -137,31 +130,10 @@ export const getOverhead = async (req, res) => {
             return res.status(400).json({ error: "Missing config or icebergPath in request body" });
         }
 
-        const result = await findTableDetails(config, icebergPath);
 
-        if (!result) {
-            return res.status(500).json({ error: "Failed to fetch table details" });
-        }
-
-        const files = result
-            .filter(item => item.size_bytes <= 100000000)
-            .map(item => ({ file_name: item.file_name, size: item.size_bytes }));
-
-        const overhead = files.map(item => ({ file_name: item.file_name, size: filesize(item.size, { standard: "jedec" }) }));
-
-        res.status(200).json({ overhead });
     } catch (error) {
         console.error("Error in getOverhead:", error.message);
         res.status(500).json({ error: "Internal server error" });
     }
 }
 
-//tests
-const config = {
-    key: "minioadmin",
-    secret: "minioadmin",
-    endpoint: "127.0.0.1:9000"
-};
-
-const icebergPath = "s3://iceberg-test/employment_data";
-// findTableDetails(config, icebergPath)
--- IcebergService/routes/keyMetrics.route.js
+++ IcebergService/routes/keyMetrics.route.js
@@ -1,9 +1,8 @@
 import express from 'express'
-import { getFileData,getOverhead,getTotalFileSize } from "../controllers/keyMetrics.controllers.js"
+import { getFileData,getOverhead } from "../controllers/keyMetrics.controllers.js"
 
 
 const router = express.Router()
-router.post("/totalSize",getTotalFileSize);
 router.post("/fileData",getFileData);
 router.post("/overhead",getOverhead);
 
